e4d778e3fce417d3ff6e3e5b1b02c599a8d66da5
kumarprem66 <premstart10@gmail.com>
2024-02-18 23:14:03 +0530

    test implemented

--- src/main/java/com/userwishlist/UserWishList/controller/WishController.java
+++ src/main/java/com/userwishlist/UserWishList/controller/WishController.java
@@ -6,22 +6,18 @@ import com.userwishlist.UserWishList.security.JwtTokenProvider;
 import com.userwishlist.UserWishList.service.CustomerService;
 import com.userwishlist.UserWishList.service.WIshListService;
 import com.userwishlist.UserWishList.utility.Common;
+import com.userwishlist.UserWishList.utility.Response;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.access.AuthorizationServiceException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import com.userwishlist.UserWishList.utility.Response;
 
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/wish")
@@ -54,6 +50,7 @@ public class WishController {
 
         String username = Common.getUserNameFromRequest(request,jwtTokenProvider);
         Customer customer = customerService.getCustomer(usernameOrEmail);
+        // checking the customer identity, whether he is same in Authorization token or not
         if(customer.getEmail().equals(username) || customer.getUsername().equals(username)){
             return new ResponseEntity<>(customerService.getCustomer(usernameOrEmail), HttpStatus.ACCEPTED);
         }else {
@@ -63,12 +60,15 @@ public class WishController {
 
     }
 
+
+
     @PostMapping("/create-wishlist/{usernameOrEmail}")
     public ResponseEntity<WishListItem> createWishList(@RequestBody WishListItem wishListItem,
                                                        @PathVariable String usernameOrEmail,HttpServletRequest request){
 
         String username = Common.getUserNameFromRequest(request,jwtTokenProvider);
         Customer customer = customerService.getCustomer(usernameOrEmail);
+        // checking the customer identity, whether he is same in Authorization token or not
         if(customer.getEmail().equals(username) || customer.getUsername().equals(username)){
             return new ResponseEntity<>(wIshListService.createWishListItem(wishListItem,usernameOrEmail), HttpStatus.CREATED);
         }else {
@@ -82,6 +82,7 @@ public class WishController {
 
         String username = Common.getUserNameFromRequest(request,jwtTokenProvider);
         Customer customer = customerService.getCustomer(usernameOrEmail);
+        // checking the customer identity, whether he is same in Authorization token or not
         if(customer.getEmail().equals(username) || customer.getUsername().equals(username)){
             return new ResponseEntity<>(wIshListService.getAllWishListByCus(usernameOrEmail), HttpStatus.ACCEPTED);
         }else {
@@ -95,6 +96,7 @@ public class WishController {
 
         String username = Common.getUserNameFromRequest(request,jwtTokenProvider);
         Customer customer = customerService.getCustomer(usernameOrEmail);
+        // checking the customer identity, whether he is same in Authorization token or not
         if(customer.getEmail().equals(username) || customer.getUsername().equals(username)){
             return new ResponseEntity<>(wIshListService.deleteWishList(wishId), HttpStatus.OK);
         }else {
--- src/main/java/com/userwishlist/UserWishList/entity/WishListItem.java
+++ src/main/java/com/userwishlist/UserWishList/entity/WishListItem.java
@@ -22,7 +22,7 @@ public class WishListItem {
     private String description;
 
     @JsonIgnore
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "customer_id", nullable = false)
     private Customer customer;
 
